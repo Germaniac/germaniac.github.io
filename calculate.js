52bcf74b22e7f726f75eb182d1c98d41e6bced4b
Germaniac <germaniachaxball@gmail.com>
2024-05-11 14:31:33 +0700

    Create calculate.js

---
+++ calculate.js
@@ -0,0 +1,11 @@
+document.getElementById('calculate').addEventListener('click', updater);
+
+function updater () {
+    var selfLP = document.getElementById('selfLP').value;
+    var oppLP = document.getElementById('oppLP').value;
+    var oppLV = document.getElementById('oppLV').value;
+    var oppATK = document.getElementById('oppATK').value;
+    var damage = 3500 + Math.max(0,oppLP-selfLP) + 200*oppLV - oppATK;
+    
+    document.getElementById('damage').innerText = damage;
+}
