6c8d3c2ba63b2fe85a706589dd0738450a8e88af
Germaniac <germaniachaxball@gmail.com>
2025-05-04 03:38:01 +0700

    Update API

--- calculate.js
+++ calculate.js
@@ -5,7 +5,7 @@ function updater (ydke) {
 		ydke: ydke
 	};
 	document.getElementById('result').innerText = 'Loading.';
-	result=fetch("https://082f957c-b392-4f8f-aa7f-4489a09215bd-00-oivlr7zrcib4.sisko.replit.dev/convert", {
+	result=fetch("https://three-abrupt-chips.glitch.me/convert", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json"
